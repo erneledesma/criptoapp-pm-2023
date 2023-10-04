d31e28ffda9c12163ea0eb0b6ee3582d1a350748
Ernesto Ledesma <eeledesma@teco.com.ar>
2023-10-04 19:10:02 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Image, View, ScrollView, ActivityIndicator } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+       <ScrollView>
+         <Header />
+
+         <View style={styles.contenido}>
+        
+        </View>
+       </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  imagen: {
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%'
   },
+  contenido: {
+    marginHorizontal: '2.5%'
+  }
 });
+
